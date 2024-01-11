3d7f99091b3dec8571cf63c453ad7b8bcc8e17d4
spring <aaa@aa.com>
2024-01-11 11:04:00 +0900

    todos add_todo

--- day09/todos1.py
+++ day09/todos1.py
@@ -3,19 +3,35 @@ todos=[]
 todos.append({'tno':1, 'title':'자바공부', 'finish':False})
 todos.append({'tno':2, 'title':'스프링 공부', 'finish':False})
 todos.append({'tno':3, 'title':'파이썬 공부', 'finish':False})
+tno = 4
+
+def print_todos():
+    for todo in todos:
+        print(todo)
+
+def add_todo():
+    title = input('할입 입력:')
+    todos.append({'tno':tno, 'title':title, 'finish':False})
+    tno=tno+1
+
+def toggle_finish():
+    pass
+
+def delete_todo():
+    pass
 
 while True:
     print('### 할일 관리 ###')
     print('1:목록, 2:추가, 3:변경, 4:삭제, 999:종료')
     select = input('메뉴 선택:')
     if select=='1':
-        print('목록')
+        print_todos()            # 호출
     elif select=='2':
-        print('추가')
+        add_todo()
     elif select=='3':
-        print('변경')
+        toggle_finish()
     elif select=='4':
-        print('삭제')
+        delete_todo()
     elif select=='999':
         print('이용해주셔서 감사합니다')
         break
